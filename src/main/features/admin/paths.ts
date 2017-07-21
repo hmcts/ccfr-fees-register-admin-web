974d61a0615a07ca71089c4f2d1a930a19d8a273
Kazys Sketrys <kazys.sketrys@triad.co.uk>
2017-07-21 13:55:04 +0100

    Mock-up page for category edit

--- src/main/features/admin/paths.ts
+++ src/main/features/admin/paths.ts
@@ -3,5 +3,6 @@ import { RoutablePath } from 'common/router/routablePath'
 export class Paths {
   static readonly welcomePage = new RoutablePath('/admin/welcome', 'admin/views/welcome')
   static readonly categoryListPage = new RoutablePath('/admin/categories', 'admin/views/category-list')
+  static readonly categoryCreatePage = new RoutablePath('/admin/categories/create', 'admin/views/category-create')
   static readonly categoryViewPage = new RoutablePath('/admin/categories/:categoryId', 'admin/views/category-view')
 }
