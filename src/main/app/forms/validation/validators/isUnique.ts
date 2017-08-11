841a3e4cc5b4f5eb7e97dc7da222d9e28d086c44
Kazys Sketrys <kazys.sketrys@triad.co.uk>
2017-08-11 15:18:23 +0100

    Removed unnecessary comment

--- src/main/app/forms/validation/validators/isUnique.ts
+++ src/main/app/forms/validation/validators/isUnique.ts
@@ -1,8 +1,5 @@
 import { registerDecorator, ValidationArguments, ValidationOptions } from 'class-validator'
 
-/**
- * Verify a valid value with minimum and maxumim digits allowed after decimal point.
- */
 export function IsUnique (uniquenessCheck: (value: any) => Promise<boolean>, validationOptions?: ValidationOptions) {
   return function (object: Object, propertyName: string) {
     registerDecorator({
