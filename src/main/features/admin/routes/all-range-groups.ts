a3effad5335b716c428377f6f3cdd3bdf77d1fe0
tpalisetty <tarun.palisetty@hmcts.net>
2017-09-29 12:43:42 +0100

    All fees view

--- src/main/features/admin/routes/all-range-groups.ts
+++ src/main/features/admin/routes/all-range-groups.ts
@@ -3,22 +3,15 @@ import * as express from 'express'
 import { Paths } from 'admin/paths'
 
 import { FeesClient } from 'app/fees/feesClient'
-import RangeGroup from 'fees/rangeGroup'
 import Category from 'fees/category'
 
 export default express.Router()
   .get(Paths.allFeesListPage.uri, (req: express.Request, res: express.Response) => {
-
     FeesClient
-      .retrieveRangeGroups()
-      .then((rangeGroups: Array<RangeGroup>) => {
-        FeesClient
-          .retrieveCategories()
-          .then((categories: Array<Category>) => {
-            res.render(Paths.allFeesListPage.associatedView, {
-              rangeGroups: rangeGroups,
-              categories: categories
-            })
-          })
+      .retrieveCategories()
+      .then((categories: Array<Category>) => {
+        res.render(Paths.allFeesListPage.associatedView, {
+          categories: categories
+        })
       })
   })
