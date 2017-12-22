e77779bed4a36ed9a978ebbbc7411d3b9a5514b7
emagdalena <eduardo.magdalena@hmcts.net>
2017-12-22 10:05:28 +0000

    PAY-519: Decimal numbers formatting (#60)

--- src/main/public/js/create-fee.js
+++ src/main/public/js/create-fee.js
@@ -1,35 +1,43 @@
 'use strict'
 
 function enableRangedFees() {
-  document.getElementById("rangeUnit").removeAttribute("disabled")
-  document.getElementById("fromRange").removeAttribute("disabled")
-  document.getElementById("toRange").removeAttribute("disabled")
+  document.getElementById("rangeUnit").removeAttribute("disabled");
+  document.getElementById("fromRange").removeAttribute("disabled");
+  document.getElementById("toRange").removeAttribute("disabled");
 }
 
 function disabledRangedFees() {
-  document.getElementById("rangeUnit").setAttribute("disabled", "disabled")
-  document.getElementById("fromRange").setAttribute("disabled", "disabled")
-  document.getElementById("toRange").setAttribute("disabled", "disabled")
+  document.getElementById("rangeUnit").setAttribute("disabled", "disabled");
+  document.getElementById("fromRange").setAttribute("disabled", "disabled");
+  document.getElementById("toRange").setAttribute("disabled", "disabled");
+}
+
+function formatDecimals(event) {
+
+  event.target.value = parseFloat(event.target.value).toFixed(2);
+
+  if (event.target.value.endsWith('.00')) {
+    event.target.value = parseInt(event.target.value);
+  }
+
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('typefixed')
-    .addEventListener('click', disabledRangedFees);
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('typeranged')
-    .addEventListener('click', enableRangedFees);
+  document.getElementById('typefixed').addEventListener('click', disabledRangedFees);
+  document.getElementById('typeranged').addEventListener('click', enableRangedFees);
+  document.getElementById('amount').onchange = formatDecimals;
+  document.getElementById('fromRange').onchange = formatDecimals;
+  document.getElementById('toRange').onchange = formatDecimals;
 });
 
 function enableFlatOrVolumeAmount() {
-  document.getElementById("amount").removeAttribute("disabled")
-  document.getElementById("percentage").setAttribute("disabled", "disabled")
+  document.getElementById("amount").removeAttribute("disabled");
+  document.getElementById("percentage").setAttribute("disabled", "disabled");
 }
 
 function enablePercentageAmount() {
-  document.getElementById("percentage").removeAttribute("disabled")
-  document.getElementById("amount").setAttribute("disabled", "disabled")
+  document.getElementById("percentage").removeAttribute("disabled");
+  document.getElementById("amount").setAttribute("disabled", "disabled");
 }
 
 document.addEventListener('DOMContentLoaded', function () {
