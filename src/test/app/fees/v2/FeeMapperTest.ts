8acb278c68543054dde962a74abe5166116a9ddf
sachi <sachi.kuppuswami@hmcts.net>
2018-06-27 17:28:08 +0100

    added more tests

--- src/test/app/fees/v2/FeeMapperTest.ts
+++ src/test/app/fees/v2/FeeMapperTest.ts
@@ -54,6 +54,83 @@ let rangedPercentageFeeCsv = {
   rangeFrom : '1',
   rangeTo : '100'
 }
+
+let bandedVolumeFeeCsv = {
+  feeVersion : '1',
+  feeDescription : 'xxx',
+  feeCode : 'xxx',
+  feeStatus : 'pending',
+  feeAmount : '100',
+  amountType : 'volume',
+  validFrom : '',
+  validTo : '',
+  rangeFrom : null,
+  rangeTo : null,
+  applicantType : 'xxx',
+  jurisdiction1 : 'xxx',
+  service : 'CMC',
+  channel : 'online',
+  jurisdiction2 : 'xxx',
+  naturalAccountCode : 'xxx',
+  event : 'xxx',
+  direction : 'xxx',
+  statutoryInstrument : 'xxx',
+  memoLine : 'xxx',
+  feeOrderName : 'xxx',
+  siRefId : 'xxx',
+  feeType : 'banded'
+}
+let rateableFlatFeeCsv = {
+  feeVersion : '1',
+  feeDescription : 'xxx',
+  feeCode : 'xxx',
+  feeStatus : 'pending',
+  feeAmount : '100',
+  amountType : 'flat',
+  validFrom : '',
+  validTo : '',
+  rangeFrom : null,
+  rangeTo : null,
+  applicantType : 'xxx',
+  jurisdiction1 : 'xxx',
+  service : 'CMC',
+  channel : 'online',
+  jurisdiction2 : 'xxx',
+  naturalAccountCode : 'xxx',
+  event : 'xxx',
+  direction : 'xxx',
+  statutoryInstrument : 'xxx',
+  memoLine : 'xxx',
+  feeOrderName : 'xxx',
+  siRefId : 'xxx',
+  feeType : 'rateable'
+}
+let relationalFlatFeeCsv = {
+  feeVersion : '1',
+  feeDescription : 'xxx',
+  feeCode : 'xxx',
+  feeStatus : 'pending',
+  feeAmount : '100',
+  amountType : 'flat',
+  validFrom : '',
+  validTo : '',
+  rangeFrom : null,
+  rangeTo : null,
+  applicantType : 'xxx',
+  jurisdiction1 : 'xxx',
+  service : 'CMC',
+  channel : 'online',
+  jurisdiction2 : 'xxx',
+  naturalAccountCode : 'xxx',
+  event : 'xxx',
+  direction : 'xxx',
+  statutoryInstrument : 'xxx',
+  memoLine : 'xxx',
+  feeOrderName : 'xxx',
+  siRefId : 'xxx',
+  feeType : 'relational'
+}
+
 describe ( 'fee-mapper', () => {
 
   describe ( 'toFixedFeeDto', () => {
@@ -63,6 +140,44 @@ describe ( 'fee-mapper', () => {
       let dto = feeMapper.toFixedFeeDto(fixedFlatFeeCsv)
 
       expect(dto.fee_type).to.equal('fixed')
+      expect(dto.version.flat_amount.amount).to.equal(100)
+
+    } )
+
+  })
+
+  describe ( 'toFixedFeeDto', () => {
+
+    it ( 'should convert to banded fee dto with the a volume fee amount', function () {
+
+      let dto = feeMapper.toFixedFeeDto(bandedVolumeFeeCsv)
+
+      expect(dto.fee_type).to.equal('banded')
+      expect(dto.version.volume_amount.amount).to.equal(100)
+
+    } )
+
+  })
+
+  describe ( 'toFixedFeeDto', () => {
+
+    it ( 'should convert to rateable fee dto with the a flat fee amount', function () {
+
+      let dto = feeMapper.toFixedFeeDto(rateableFlatFeeCsv)
+
+      expect(dto.fee_type).to.equal('rateable')
+      expect(dto.version.flat_amount.amount).to.equal(100)
+
+    } )
+
+  })
+  describe ( 'toFixedFeeDto', () => {
+
+    it ( 'should convert to relational fee dto with the a flat fee amount', function () {
+
+      let dto = feeMapper.toFixedFeeDto(relationalFlatFeeCsv)
+
+      expect(dto.fee_type).to.equal('relational')
       expect(dto.version.flat_amount.amount).to.equal(100)
 
     } )
