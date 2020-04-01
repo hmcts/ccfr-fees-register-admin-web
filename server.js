b9a724213236cfd31cf617ad7df8f1312457ad33
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2020-04-01 19:06:39 +0100

    remove unused

--- server.js
+++ server.js
@@ -2,4 +2,3 @@ process.env.TS_NODE_FAST = "true"
 require('ts-node/register')
 require('tsconfig-paths/register')
 require('./src/main/server.ts')
-require('@hmcts/properties-volume').addTo(require('config'))
