65eb22982cbae01c5c74e3ff144cca725e743a92
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2020-04-01 16:32:51 +0100

    lint

--- src/test/modules/secrets/secrets.ts
+++ src/test/modules/secrets/secrets.ts
@@ -1,33 +1,33 @@
-import * as config from 'config';
-import { expect } from 'chai';
-import * as secrets from '../../../main/modules/secrets';
-import { set, remove, cloneDeep } from 'lodash';
+import * as config from 'config'
+import { expect } from 'chai'
+import * as secrets from '../../../main/modules/secrets'
+import { set, remove, cloneDeep } from 'lodash'
 
-const APP_INSIGHT_IKEY_CONFIG_PATH = 'secrets.ccpay.AppInsightsInstrumentationKey';
-const APP_INSIGHT_IKEY = '1';
+const APP_INSIGHT_IKEY_CONFIG_PATH = 'secrets.ccpay.AppInsightsInstrumentationKey'
+const APP_INSIGHT_IKEY = '1'
 
 describe('application insight setup', () => {
-  let configuration;
+  let configuration
 
   beforeEach(() => {
-    configuration = cloneDeep(config);
-  });
+    configuration = cloneDeep(config)
+  })
 
   it('should add application insight secrets to config', () => {
 
-    set(configuration, APP_INSIGHT_IKEY_CONFIG_PATH, APP_INSIGHT_IKEY);
+    set(configuration, APP_INSIGHT_IKEY_CONFIG_PATH, APP_INSIGHT_IKEY)
 
-    secrets.setup(configuration);
+    secrets.setup(configuration)
 
-    expect(configuration.appInsights.instrumentationKey).to.equal(APP_INSIGHT_IKEY);
-  });
+    expect(configuration.appInsights.instrumentationKey).to.equal(APP_INSIGHT_IKEY)
+  })
 
   it('should not add application insight secrets to config if secrets is absent', () => {
-    remove(configuration, APP_INSIGHT_IKEY_CONFIG_PATH);
+    remove(configuration, APP_INSIGHT_IKEY_CONFIG_PATH)
 
-    secrets.setup(configuration);
+    secrets.setup(configuration)
 
-    expect(configuration.applicationInsights).to.be.undefined;
-  });
+    expect(configuration.applicationInsights).to.equal(undefined)
+  })
 
-});
+})
