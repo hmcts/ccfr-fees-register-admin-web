6e4874f984ad8a95b000f49d28f6db24ba4d60fc
Satish Ramachandrappa <satish.ramachandrappa@hmcts.net>
2021-03-26 14:30:23 +0000

    Reverting date change as in master

--- acceptance-tests/test/end-to-end/pages/steps_file.js
+++ acceptance-tests/test/end-to-end/pages/steps_file.js
@@ -30,33 +30,33 @@ module.exports = () => actor({
     this.wait(CCPBConstants.fiveSecondWaitTime);
   },
 
-  getDateInDDMMYYYY(date = new Date()) {
-    const stringFillSize = 2;
-    const day = date.getDate()
-      .toString()
-      .padStart(stringFillSize, '0');
-    const month = (date.getMonth() + 1).toString()
-      .padStart(stringFillSize, '0');
-    const year = date.getFullYear()
-      .toString();
-    return `${day}/${month}/${year}`;
-  },
+  // getDateInDDMMYYYY(date = new Date()) {
+  //   const stringFillSize = 2;
+  //   const day = date.getDate()
+  //     .toString()
+  //     .padStart(stringFillSize, '0');
+  //   const month = (date.getMonth() + 1).toString()
+  //     .padStart(stringFillSize, '0');
+  //   const year = date.getFullYear()
+  //     .toString();
+  //   return `${day}/${month}/${year}`;
+  // },
 
   addNewFee(feeKeyword) {
 
     const memoLineNumber = faker.random.number(RANDOM_NUMBER);
     const naturalAccountCode = faker.random.number(RANDOM_NUMBER);
 
-    const fromDate = this.getDateInDDMMYYYY();
-    const futureDate = new Date();
-    futureDate.setMonth(futureDate.getMonth() + 3)
-    const toDate = this.getDateInDDMMYYYY(futureDate);
+    const fromDate = new Date();
+    const toDate = new Date();
+    toDate.setMonth(toDate.getMonth() + 3);
+
     this.click('Add a new fee');
     this.fillField({ css: '#memoLine'}, memoLineNumber);
     this.fillField({ css: '#naturalAccountCode'}, '232425');
     this.fillField({ css: '#description'}, feeKeyword);
-    this.fillField({ css: '#fromDate'}, fromDate);
-    this.fillField({ css: '#toDate'}, toDate);
+    this.fillField({ css: '#fromDate'}, fromDate.toLocaleDateString('en-GB'));
+    this.fillField({ css: '#toDate'}, toDate.toLocaleDateString('en-GB'));
     this.fillField({ css: '#amount'}, 120.00);
     this.selectOption({ css: '#applicantType' }, 'all');
     this.selectOption({ css: '#jurisdiction1' }, 'family');
