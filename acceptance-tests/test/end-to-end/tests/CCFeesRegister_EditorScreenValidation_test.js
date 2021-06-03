6076bbe26b1bea6aa4b17fe4ee7e665f4e854a94
Pettedson John <pettedson.john@hmcts.net>
2021-06-03 15:41:14 +0100

    Adding the e2e Functional Tests for the Live Fees.....

--- acceptance-tests/test/end-to-end/tests/CCFeesRegister_EditorScreenValidation_test.js
+++ acceptance-tests/test/end-to-end/tests/CCFeesRegister_EditorScreenValidation_test.js
@@ -1,16 +1,17 @@
+const { Logger } = require('@hmcts/nodejs-logging');
 const CCFRATConstants = require('./CCFRAcceptanceTestConstants');
 const faker = require('faker');
 const RANDOM_NUMBER = 9999;
 
-Feature('CC FeesRegister Admin Acceptance Tests').retry(CCFRATConstants.retryScenario);
+Feature('CC FeesRegister Admin Acceptance Tests For Live Fees').retry(CCFRATConstants.retryScenario);
 
 Scenario('FeesRegister Admin Console Editor Screen For Live Fees', I => {
   I.login('functionaltesteditor@hmcts.net', 'LevelAt12');
   I.wait(CCFRATConstants.tenSecondWaitTime);
-  I.waitForText('Live fees', CCFRATConstants.tenSecondWaitTime);
-  I.waitForText('Code', CCFRATConstants.tenSecondWaitTime);
-  I.waitForText('Jurisdiction2', CCFRATConstants.tenSecondWaitTime);
   // to-do based on updates and future stories
+  Logger.info('Verifying the Live Fees ');
+  I.verifyLiveFees();
+  Logger.info('Live Fees page Verified');
   I.Logout("editor");
 });
 
