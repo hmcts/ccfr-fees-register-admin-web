35ab3f4df9a154563ee9cf6b8cb2f81865fc058e
“Kumar <Kumarmcamadura@gmail.com>
2021-06-07 17:38:07 +0100

    PAY-4592 Changes

--- src/main/routes/welcome.ts
+++ src/main/routes/welcome.ts
@@ -2,15 +2,10 @@ import * as express from 'express'
 
 export default express.Router()
   .get('/', function (req, res) {
-    const roles = res.locals.user.userInfo;
-    
-    if(roles.indexOf('freg-approver') !== -1) {
+    const roles = res.locals.user.userInfo
+    if (roles.indexOf('freg-approver') !== -1) {
       res.redirect('/admin/V2/pending-approval')
     } else {
       res.redirect('/admin/V2/all-fees')
     }
   })
-
-
-
-
