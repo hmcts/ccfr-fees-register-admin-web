8ae9ccffb04f67afb0f978e5f1d0b2919f56d4c8
“Kumar <Kumarmcamadura@gmail.com>
2021-06-08 10:12:28 +0100

    PAY-4592 Changes

--- src/main/features/admin/v2/routes/pending-approval.ts
+++ src/main/features/admin/v2/routes/pending-approval.ts
@@ -9,7 +9,7 @@ import { Fee2Dto } from 'fees/v2/model/fees-register-api-contract'
 export default express.Router()
   .get(Paths.pendingApprovalPageV2.uri, (req: express.Request, res: express.Response) => {
     FeesClient
-      .searchFees('pending_approval', null, null, null, null, false)
+      .searchFees(null, null, null, false, null, false)
       .then((fees: Array<Fee2Dto>) => {
         fees.sort((a: Fee2Dto, b: Fee2Dto): number => -a.code.localeCompare(b.code))
         res.render(Paths.pendingApprovalPageV2.associatedView, {
