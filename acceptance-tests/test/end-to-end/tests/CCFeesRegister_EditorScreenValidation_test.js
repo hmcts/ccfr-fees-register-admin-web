b117d912f033310b9076c6339225fa5cffb6e65f
“Kumar <Kumarmcamadura@gmail.com>
2021-06-23 16:17:43 +0100

    build issue fixes

--- acceptance-tests/test/end-to-end/tests/CCFeesRegister_EditorScreenValidation_test.js
+++ acceptance-tests/test/end-to-end/tests/CCFeesRegister_EditorScreenValidation_test.js
@@ -8,8 +8,49 @@ Feature('CC FeesRegister Admin Acceptance Tests For Live Fees').retry(CCFRATCons
 Scenario('FeesRegister Admin Console Editor Header Validation', I => {
   I.login('functionaltesteditor@hmcts.net', 'LevelAt12');
   I.wait(CCFRATConstants.tenSecondWaitTime);
-  I.see("Fees");
-  I.see("Your Drafts");
-  I.see("Reference Data");
-  I.click('Sign out');
+  // to-do based on updates and future stories
+  I.verifyLiveFees();
+  I.Logout("editor");
+});
+
+Scenario('FeesRegister Admin Console Editor Screen For Approved but not live fees', I => {
+  I.login('functionaltesteditor@hmcts.net', 'LevelAt12');
+  I.wait(CCFRATConstants.tenSecondWaitTime);
+  I.waitForText('Approved but not live fees', CCFRATConstants.tenSecondWaitTime);
+  I.click('Approved but not live fees');
+  I.waitForText('Code', CCFRATConstants.tenSecondWaitTime);
+  I.waitForText('Description', CCFRATConstants.tenSecondWaitTime);
+  I.waitForText('Jurisdiction2', CCFRATConstants.tenSecondWaitTime);
+  // to-do based on updates and future stories
+  I.Logout("editor");
+});
+
+Scenario('FeesRegister Admin Console Editor Screen For Discontinued fees', I => {
+  I.login('functionaltesteditor@hmcts.net', 'LevelAt12');
+  I.wait(CCFRATConstants.tenSecondWaitTime);
+  I.waitForText('Discontinued fees', CCFRATConstants.tenSecondWaitTime);
+  I.click('Discontinued fees');
+  I.waitForText('Code', CCFRATConstants.tenSecondWaitTime);
+  I.waitForText('Description', CCFRATConstants.tenSecondWaitTime);
+  I.waitForText('Jurisdiction2', CCFRATConstants.tenSecondWaitTime);
+  I.waitForText('Valid to', CCFRATConstants.tenSecondWaitTime);
+  // to-do based on updates and future stories
+  //Check one of the existing fee, once full implementation done we can add our own code
+  I.waitForText('FEE0565', CCFRATConstants.tenSecondWaitTime);
+  I.see('Version1');
+  I.see('Version2');
+  I.click('FEE0565');
+  I.waitForText('Fee details', CCFRATConstants.tenSecondWaitTime);
+  I.Logout("editor");
+});
+
+Scenario('FeesRegister Add New Fee and Submit for Approval', I => {
+  const feeKeyword = "SN" + new Date().valueOf().toString();
+  const submitBtnVisibilityChk = true;
+
+  I.login('functionaltesteditor@hmcts.net', 'LevelAt12');
+  I.wait(CCFRATConstants.twoSecondWaitTime);
+  I.waitForText('Live fees', CCFRATConstants.tenSecondWaitTime);
+  // to-do based on updates and future stories
+  I.Logout("editor");
 });
