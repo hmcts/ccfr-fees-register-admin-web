0af0d4eb0502b7af67d96cba90b5d52cc96a986f
“Kumar <Kumarmcamadura@gmail.com>
2021-06-23 14:32:19 +0100

    fee details page changes

--- src/main/app/fees/v2/forms/model/RejectFeeform.ts
+++ src/main/app/fees/v2/forms/model/RejectFeeform.ts
@@ -3,46 +3,44 @@ import { ValidationErrors } from 'fees/v2/forms/model/ValidationErrors'
 import { ReasonDto } from 'app/fees/v2/model/fees-register-api-contract'
 
 export class RejectFeeForm {
-    code?: string
+  code?: string
 
-    @IsOptional()
-    @MaxLength(1000, { message: ValidationErrors.REASON_FOR_REJECTION_TOO_LONG })
-    @Matches(/^([a-zA-Z0-9]*)$/, {
-        message: ValidationErrors.ALPHA_NUMERIC
-    })
-    reasonForReject?: string
+  @IsOptional()
+  @MaxLength(1000, { message: ValidationErrors.REASON_FOR_REJECTION_TOO_LONG })
+  @Matches(/^([a-zA-Z0-9]*)$/, {
+    message: ValidationErrors.ALPHA_NUMERIC
+  })
+  reasonForReject?: string
 
-    constructor() {
-        this.code = null
-        this.reasonForReject = ''
+  constructor () {
+    this.code = null
+    this.reasonForReject = ''
+  }
+
+  static fromObject (value?: any): RejectFeeForm {
+
+    if (!value) {
+      return new RejectFeeForm()
     }
 
-    static fromObject(value?: any): RejectFeeForm {
+    const form: RejectFeeForm = new RejectFeeForm()
+    Object.keys(value).forEach(key => form[key] = value[key])
+    return form
+  }
 
-        if (!value) {
-            return new RejectFeeForm()
-        }
+  toDto (): ReasonDto {
 
-        const form: RejectFeeForm = new RejectFeeForm()
-        Object.keys(value).forEach(key => form[key] = value[key])
+    const dto = new ReasonDto()
+    dto.reasonForReject = this.reasonForReject
 
+    return this.fillCommon(dto)
 
-        return form
-    }
+  }
 
-    toDto(): ReasonDto {
+  private fillCommon (dto: ReasonDto): ReasonDto {
 
-        const dto = new ReasonDto()
-        dto.reasonForReject = this.reasonForReject
+    dto.reasonForReject = this.reasonForReject
 
-        return this.fillCommon(dto)
-
-    }
-
-    private fillCommon(dto: ReasonDto): ReasonDto {
-
-        dto.reasonForReject = this.reasonForReject
-
-        return dto;
-    }
+    return dto
+  }
 }
--- src/main/features/admin/v2/routes/reject-fee-reason.ts
+++ src/main/features/admin/v2/routes/reject-fee-reason.ts
@@ -8,41 +8,33 @@ import { FeesClient } from 'app/fees/v2/feesClient'
 
 import { FormValidator } from 'app/forms/validation/formValidator'
 import { RejectFeeForm } from 'app/fees/v2/forms/model/RejectFeeform'
-import { ReasonDto } from 'app/fees/v2/model/fees-register-api-contract'
-
 
 class Renderer {
-    static renderPage(form: Form<RejectFeeForm>, res: express.Response) {
-
-        res.render(Paths.feeRejectReason.associatedView,
-            {
-                form: form
-            })
-
-    }
+  static renderPage (form: Form<RejectFeeForm>, res: express.Response) {
+    res.render(Paths.feeRejectReason.associatedView, {
+      form: form
+    })
+  }
 }
 
 export default express.Router()
-    .get(Paths.feeRejectReason.uri, (req: express.Request, res: express.Response) => {
-        Renderer.renderPage(new Form(new RejectFeeForm()), res)
-    })
+  .get(Paths.feeRejectReason.uri, (req: express.Request, res: express.Response) => {
+    Renderer.renderPage(new Form(new RejectFeeForm()), res)
+  })
 
-    .post(Paths.feeRejectReason.uri, FormValidator.requestHandler(RejectFeeForm, RejectFeeForm.fromObject), (req: express.Request, res: express.Response) => {
-        const form: Form<RejectFeeForm> = req.body
-
-        if (form.hasErrors()) {
-            Renderer.renderPage(form, res)
-        } else {
-            FeesClient.reasonForRejectFee(res.locals.user, req.url.split("/")[4], Number(req.url.split("/")[5]), form.model.toDto() as ReasonDto).then(
-                () =>
-                    res.render('admin/v2/views/fee-rejected-confirmation', {
-                        feeCode: form.model.code
-                    })
-            ).catch(
-                (e: Error) => {
-                    form.backendErrors.push(e.message)
-                    Renderer.renderPage(form, res)
-                }
-            )
-        }
-    })
+  .post(Paths.feeRejectReason.uri, FormValidator.requestHandler(RejectFeeForm, RejectFeeForm.fromObject), (req: express.Request, res: express.Response) => {
+    const form: Form<RejectFeeForm> = req.body
+    if (form.hasErrors()) {
+      Renderer.renderPage(form, res)
+    } else {
+      FeesClient.reasonForRejectFee(res.locals.user, req.url.split('/')[4], Number(req.url.split('/')[5]), form.model.toDto())
+      .then(() =>
+        res.render('admin/v2/views/fee-rejected-confirmation', {
+          feeCode: form.model.code
+        })
+      ).catch((e: Error) => {
+        form.backendErrors.push(e.message)
+        Renderer.renderPage(form, res)
+      })
+    }
+  })
