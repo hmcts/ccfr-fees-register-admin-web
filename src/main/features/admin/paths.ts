84f756eb437f4f01978625c15f6efac1076171da
priyatham-bhikkumalla <priyatham.bhikkumalla@hmcts.net>
2021-06-28 11:42:09 +0100

    Reaon for fee raejection

--- src/main/features/admin/paths.ts
+++ src/main/features/admin/paths.ts
@@ -40,6 +40,7 @@ export class Paths {
   static readonly discontinuedFeesPageV2 = new RoutablePath('/admin/v2/all-discontinued', 'admin/v2/views/all-discontinued')
   static readonly feeCreatePageV2 = new RoutablePath('/admin/v2/fees/create', 'admin/v2/views/create-edit-fee')
   static readonly feeDetailsViewPagev2 = new RoutablePath('/admin/v2/fee-details', 'admin/v2/views/fee-details')
+  static readonly feeRejectReason = new RoutablePath('/admin/v2/reject-fee-reason/:feeCode/:feeVersion', 'admin/v2/views/reject-fee-reason')
 
   static readonly createFeeVersionPageV2 = new RoutablePath('/admin/v2/fees/version/create/:feeCode', 'admin/v2/views/create-fee-version')
   static readonly dashboard = new RoutablePath('/admin/v2/dashboard', 'admin/v2/views/dashboard')
