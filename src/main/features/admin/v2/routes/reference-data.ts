10feacf643d97aa680bacc89729a589141426f62
thomasbennett-hmcts <thomas.bennett@hmcts.net>
2021-06-29 15:01:12 +0100

    added other data types

--- src/main/features/admin/v2/routes/reference-data.ts
+++ src/main/features/admin/v2/routes/reference-data.ts
@@ -3,18 +3,20 @@ import * as express from 'express'
 import { Paths } from 'admin/paths'
 
 import { FeesClient } from 'app/fees/v2/feesClient'
-import { ApplicantTypeDto } from 'app/fees/v2/model/fees-register-api-contract'
-// import { ServiceTypeDto } from 'fees/v2/model/fees-register-api-contract'
 
 export default express.Router()
 .get(Paths.referenceDataPage.uri, (req: express.Request, res: express.Response) => {
-  FeesClient.retrieveReferenceData().then(
-      data => {
-        res.render(Paths.referenceDataPage.associatedView,
-            {
-              referenceData: data,
-              applicants: ApplicantTypeDto,
-    
-            })
-          })
+  FeesClient.retrieveReferenceData()
+  .then(data => {
+    res.render(Paths.referenceDataPage.associatedView, {
+        referenceData: data,
+        applicants: data.applicantTypes,
+        jurisdictions1: data.jurisdictions1,
+        jurisdictions2: data.jurisdictions2,
+        services: data.serviceTypes,
+        channels: data.channelTypes,
+        events: data.eventTypes,
+        directions: data.directionTypes
+        })
+    })
 })
