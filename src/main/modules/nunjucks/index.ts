1630165769f8093d923b63655d6cecde49e4c5a1
kumarmadurai <kumarmcamadurai@gmail.com>
2021-06-29 16:32:14 +0100

    discon fee changes

--- src/main/modules/nunjucks/index.ts
+++ src/main/modules/nunjucks/index.ts
@@ -133,13 +133,10 @@ export default class Nunjucks {
 
     nunjucksEnv.addGlobal('getSelectedVersion', (fee: Fee2Dto, vno: any): FeeVersionDto => {
       if (fee.fee_versions != null) {
-        let result: FeeVersionDto
-        fee.fee_versions.forEach((fv) => {
-          if (fv.status === 'approved' && new String(fv.version) === new String(vno)) {
-            result = fv
-          }
+        let result: FeeVersionDto = fee.fee_versions.find((fv) => {
+          return fv.version === vno
         })
-        return result
+        
       }
     })
 
