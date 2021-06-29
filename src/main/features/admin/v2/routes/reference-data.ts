2ecc30ed69ba7d4dd117da11fb24e74e8bcf8f46
thomasbennett-hmcts <thomas.bennett@hmcts.net>
2021-06-29 08:59:28 +0100

    changing route to test page

--- src/main/features/admin/v2/routes/reference-data.ts
+++ src/main/features/admin/v2/routes/reference-data.ts
@@ -2,19 +2,32 @@ import * as express from 'express'
 
 import { Paths } from 'admin/paths'
 
-import { FeesClient } from 'app/fees/v2/feesClient'
+// import { FeesClient } from 'app/fees/v2/feesClient'
 // import { ApplicantTypeDto } from 'fees/v2/model/fees-register-api-contract'
-import { ServiceTypeDto } from 'fees/v2/model/fees-register-api-contract'
+// import { ServiceTypeDto } from 'fees/v2/model/fees-register-api-contract'
 
 export default express.Router()
-.get(Paths.serviceListPage.uri, (req: express.Request, res: express.Response) => {
-
-  FeesClient
-      .retrieveServices()
-      .then((services: Array<ServiceTypeDto>) => {
-
-        res.render(Paths.serviceListPage.associatedView, {
-          services: services
-        })
-      })
+.get(Paths.referenceDataPage.uri, (req: express.Request, res: express.Response) => {
+  res.render(Paths.referenceDataPage.associatedView, { roles: res.locals.user.userInfo })
 })
+// .get(Paths.applicantListPage.uri, (req: express.Request, res: express.Response) => {
+
+//     FeesClient
+//       .retrieveApplicants()
+//       .then((applicants: Array<ApplicantTypeDto>) => {
+//         res.render(Paths.applicantListPage.associatedView, {
+//           applicants: applicants
+//         })
+//       })
+//   })
+//   .get(Paths.serviceListPage.uri, (req: express.Request, res: express.Response) => {
+
+//     FeesClient
+//       .retrieveServices()
+//       .then((services: Array<ServiceTypeDto>) => {
+
+//         res.render(Paths.serviceListPage.associatedView, {
+//           services: services
+//         })
+//       })
+//   })
