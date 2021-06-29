5fd2a69c2e3e9f9e026d54f0c6e325f1195de8c8
thomasbennett-hmcts <thomas.bennett@hmcts.net>
2021-06-29 10:48:53 +0100

    used create edit fee as example for reference data

--- src/main/features/admin/v2/routes/reference-data.ts
+++ src/main/features/admin/v2/routes/reference-data.ts
@@ -3,39 +3,17 @@ import * as express from 'express'
 import { Paths } from 'admin/paths'
 
 import { FeesClient } from 'app/fees/v2/feesClient'
-import { ApplicantTypeDto } from 'fees/v2/model/fees-register-api-contract'
+// import { AllReferenceDataDto, ApplicantTypeDto } from 'app/fees/v2/model/fees-register-api-contract'
+// import { ServiceTypeDto } from 'fees/v2/model/fees-register-api-contract'
 
 export default express.Router()
 .get(Paths.referenceDataPage.uri, (req: express.Request, res: express.Response) => {
-
-  FeesClient
-    // .retrieveReferenceData
-    .retrieveApplicants()
-    .then((applicants: Array<ApplicantTypeDto>) => {
-      res.render(Paths.referenceDataPage.associatedView, {
-        roles: res.locals.user.userInfo,
-        applicants: applicants
-      })
-    })
+  FeesClient.retrieveReferenceData().then(
+      data => {
+        res.render(Paths.referenceDataPage.associatedView,
+            {
+              referenceData: data
+            })
+          })
 })
-// .get(Paths.applicantListPage.uri, (req: express.Request, res: express.Response) => {
 
-//     FeesClient
-//       .retrieveApplicants()
-//       .then((applicants: Array<ApplicantTypeDto>) => {
-//         res.render(Paths.applicantListPage.associatedView, {
-//           applicants: applicants
-//         })
-//       })
-//   })
-//   .get(Paths.serviceListPage.uri, (req: express.Request, res: express.Response) => {
-
-//     FeesClient
-//       .retrieveServices()
-//       .then((services: Array<ServiceTypeDto>) => {
-
-//         res.render(Paths.serviceListPage.associatedView, {
-//           services: services
-//         })
-//       })
-//   })
