579eebc46b249b5e363c872619e1f017a912644c
thomasbennett-hmcts <thomas.bennett@hmcts.net>
2021-06-29 08:50:00 +0100

    changing route to test page

--- src/main/features/admin/v2/routes/reference-data.ts
+++ src/main/features/admin/v2/routes/reference-data.ts
@@ -3,26 +3,13 @@ import * as express from 'express'
 import { Paths } from 'admin/paths'
 
 import { FeesClient } from 'app/fees/v2/feesClient'
+// import { ApplicantTypeDto } from 'fees/v2/model/fees-register-api-contract'
 import { ServiceTypeDto } from 'fees/v2/model/fees-register-api-contract'
-// import { AllReferenceDataDto } from 'fees/v2/model/fees-register-api-contract'
-
-// export default express.Router()
-// .get(Paths.referenceDataPage.uri, (req: express.Request, res: express.Response) => {
-
-//   FeesClient
-//       .retrieveReferenceData()
-//     //   .then((references: <AllReferenceDataDto>) => {
-
-//         // res.render(Paths.referenceDataPage.associatedView, {
-//         //   reference: references
-//         // })
-// })
-//     // }
 
 export default express.Router()
-  .get(Paths.serviceListPage.uri, (req: express.Request, res: express.Response) => {
+.get(Paths.serviceListPage.uri, (req: express.Request, res: express.Response) => {
 
-    FeesClient
+  FeesClient
       .retrieveServices()
       .then((services: Array<ServiceTypeDto>) => {
 
@@ -30,4 +17,4 @@ export default express.Router()
           services: services
         })
       })
-  })
+})
