fa1379ca56207538b7c47e9df1c9cfeaa05a621d
kumarmadurai <kumar.dhanasamy@hmcts.net>
2021-07-02 09:56:41 +0100

    Fee-details fixes

--- src/main/modules/nunjucks/index.ts
+++ src/main/modules/nunjucks/index.ts
@@ -135,7 +135,7 @@ export default class Nunjucks {
       if (fee.fee_versions != null) {
         let result: FeeVersionDto
         fee.fee_versions.forEach((fv: FeeVersionDto) => {
-          if (fv.status === 'approved' && fv.version === vno) {
+          if (fv.version === vno && fv.status === 'approved') {
             result = fv
           }
         })
