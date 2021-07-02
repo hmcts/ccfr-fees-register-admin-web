37da539648cb5a4272cb1c7a15a9e0ab8619c242
santu412 <490436@ambgb000021.home>
2021-07-02 15:46:56 +0100

    PAY-4567

---
+++ src/main/features/admin/v2/routes/confirm-draft-approval.ts
@@ -0,0 +1,28 @@
+import * as express from 'express'
+
+import { Paths } from '../../../admin/paths'
+
+// import { FeesClient } from '../../../../app/fees/v2/feesClient'
+
+// import { AuthOptions } from 'request'
+
+// class Renderer {
+//     static render(res: express.Response): any {
+//         throw new Error("Method not implemented.");
+//     }
+// static executeAction(user: AuthOptions, action: string, feeCode: string, version: number) {
+//      return FeesClient.approveFee(user, feeCode, version)
+// }
+// }
+
+export default express.Router()
+.get(Paths.draftViewV2.uri, (req: express.Request, res: express.Response) => {
+  console.log('one')
+  res.render(Paths.draftViewV2.associatedView)
+})
+
+  // .post(Paths.dashboard.uri, (req: express.Request, res: express.Response) => {
+  //   Renderer.executeAction(res.locals.user, req.body.action, req.body.feeCode, req.body.version)
+  //     .then(() => Renderer.render(res))
+  //     .catch(() => Renderer.render(res))
+  // })
