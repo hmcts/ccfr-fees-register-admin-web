d68b5771aa4da3bc7aaee2e5b8442305f254d3c3
priyatham-bhikkumalla <priyatham.bhikkumalla@hmcts.net>
2021-07-05 18:31:50 +0100

    fee versions - first cut

---
+++ src/main/features/admin/v2/routes/fee-versions.ts
@@ -0,0 +1,17 @@
+import * as express from 'express'
+
+import { Paths } from 'admin/paths'
+
+import { FeesClient } from 'app/fees/v2/feesClient'
+
+import { Fee2Dto } from 'fees/v2/model/fees-register-api-contract'
+
+export default express.Router()
+    .get(Paths.feeVersions.uri, (req: express.Request, res: express.Response) => {
+      FeesClient
+          .getFee(req.params.feeCode)
+            .then((feeDto: Fee2Dto) => {
+              res.render(Paths.feeVersions.associatedView, { feeDto: feeDto })
+            })
+
+    })
