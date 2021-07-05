29adf99edfb69c254d77fd1bc34097216cc5ae3d
kumarmadurai <kumar.dhanasamy@hmcts.net>
2021-07-05 22:18:00 +0100

    Awaiting approval changes

--- src/main/features/admin/paths.ts
+++ src/main/features/admin/paths.ts
@@ -41,6 +41,7 @@ export class Paths {
   static readonly discontinuedFeesPageV2 = new RoutablePath('/admin/v2/all-discontinued', 'admin/v2/views/all-discontinued')
   static readonly feeCreatePageV2 = new RoutablePath('/admin/v2/fees/create', 'admin/v2/views/create-edit-fee')
   static readonly feeDetailsViewPagev2 = new RoutablePath('/admin/v2/fee-details', 'admin/v2/views/fee-details')
+  static readonly approvalConfirmationPageV2 = new RoutablePath('/admin/v2/approval-confirmation', 'admin//v2/views/approval-confirmation')
 
   static readonly createFeeVersionPageV2 = new RoutablePath('/admin/v2/fees/version/create/:feeCode', 'admin/v2/views/create-fee-version')
   static readonly dashboard = new RoutablePath('/admin/v2/dashboard', 'admin/v2/views/dashboard')
