f385b1d2805f4982a48ae187e5ed68bf92a56287
kumarmadurai <kumar.dhanasamy@hmcts.net>
2021-07-09 13:26:03 +0100

    draft related issue fixes

--- src/main/app/fees/v2/forms/model/CreateFeeVersionForm.ts
+++ src/main/app/fees/v2/forms/model/CreateFeeVersionForm.ts
@@ -9,11 +9,16 @@ import { ValidationErrors } from 'fees/v2/forms/model/ValidationErrors'
 import {
   FeeVersionDto, FlatAmountDto, PercentageAmountDto, VolumeAmountDto
 } from 'fees/v2/model/fees-register-api-contract'
+const reasonForUpdateMsg = { message: ValidationErrors.REASON_FOR_UPDATE_REQUIRED }
 
 export class CreateFeeVersionForm {
   @IsDefined({ message: ValidationErrors.TYPE_REQUIRED })
   @IsNotBlank({ message: ValidationErrors.TYPE_REQUIRED })
   amountType?: string
+
+  @IsDefined(reasonForUpdateMsg)
+  @IsNotBlank(reasonForUpdateMsg)
+  reasonForUpdate?: string
 
   @IsDefined({ message: ValidationErrors.DESCRIPTION_REQUIRED })
   @IsNotBlank({ message: ValidationErrors.DESCRIPTION_REQUIRED })
@@ -62,6 +67,7 @@ export class CreateFeeVersionForm {
   constructor () {
     this.amountType = 'flat'
     this.description = ''
+    this.reasonForUpdate = ''
     this.memoLine = ''
     this.direction = ''
   }
@@ -130,6 +136,8 @@ export class CreateFeeVersionForm {
 
     dto.natural_account_code = this.naturalAccountCode
     dto.statutory_instrument = this.statutoryInstrument
+    dto.reason_for_update = this.reasonForUpdate
+
     dto.si_ref_id = this.siRefId
     dto.fee_order_name = this.feeOrderName
     dto.description = this.description
