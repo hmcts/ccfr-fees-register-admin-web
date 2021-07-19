9ca45ef2c3f0606a1b4adf7ac8c65a9a8a0ea321
kumarmadurai <kumar.dhanasamy@hmcts.net>
2021-07-19 12:23:27 +0100

    fraft issue fixes

--- src/main/features/admin/v2/routes/fee-details.ts
+++ src/main/features/admin/v2/routes/fee-details.ts
@@ -5,6 +5,19 @@ import { Paths } from 'admin/paths'
 import { FeesClient } from 'app/fees/v2/feesClient'
 
 import { Fee2Dto } from 'fees/v2/model/fees-register-api-contract'
+import { AuthOptions } from 'request'
+
+class Renderer {
+
+  static executeAction (user: AuthOptions, action: string, feeCode: string, version: number): Promise<Boolean> {
+    switch (action) {
+      case 'approve':
+        return FeesClient.approveFee(user, feeCode, version)
+      case 'submit':
+        return FeesClient.submitForReview(user, feeCode, version)
+    }
+  }
+}
 
 export default express.Router()
   .get(Paths.feeDetailsViewPagev2.uri, (req: express.Request, res: express.Response) => {
@@ -19,6 +32,12 @@ export default express.Router()
       })
   })
   .post(Paths.feeDetailsViewPagev2.uri, (req: express.Request, res: express.Response) => {
-    FeesClient.approveFee(res.locals.user, req.body.feeCode, req.body.version)
-      .then(() => res.redirect(`/admin/v2/approval-confirmation?feeCode=${req.query.feeCode}`))
+    Renderer.executeAction(res.locals.user, req.body.action, req.body.feeCode, req.body.version)
+      .then(() => {
+        if(req.body.action === 'approve') {
+          res.redirect(`/admin/v2/approval-confirmation?feeCode=${req.query.feeCode}`)
+        } else if(req.body.action === 'submit') {
+          res.redirect(`/admin/v2/approval-request-confirmation?feeCode=${req.query.feeCode}`)
+        }
+      })
   })
