fe6e3e31d4bc8cea1dbd0cef59ac7213de66b861
vianvi <30834324+vianvi@users.noreply.github.com>
2021-07-21 15:55:49 +0100

    add Tab and header link verification tests

--- acceptance-tests/test/end-to-end/tests/CCFeesRegister_ApproverScreenValidation_test.js
+++ acceptance-tests/test/end-to-end/tests/CCFeesRegister_ApproverScreenValidation_test.js
@@ -4,16 +4,28 @@ const RANDOM_NUMBER = 9999;
 
 Feature('CC FeesRegister Admin Acceptance Tests');
 
-Scenario('FeesRegister Admin Console Approver Screen Validation @crossbrowser', I => {
+Scenario('FeesRegister Admin Console Approver Header and Tab Validation', I => {
   I.login('functionaltestapprover@hmcts.net', 'LevelAt12');
   I.wait(CCFRATConstants.tenSecondWaitTime);
   I.see("Fees");
+  I.click("Fees");
+  I.waitForText("Live fees","10");
+  I.click("Approved but not live fees");
+  I.waitForText("Approved but not live fees","10");
+  I.see("Code");
+  I.click("Discontinued fees");
+  I.waitForText("Discontinued fees","10");
+  I.see("Code");
   I.see("Approvals");
+  I.click("Approvals");
+  I.waitForText("Awaiting approval", "10");
   I.see("Reference Data");
+  I.click("Reference Data");
+  I.waitForText("Applicants","10");
   I.click('Sign out');
 }).retry(CCFRATConstants.retryScenario)
 
-Scenario('FeesRegister Verify Pending For Approval', async I => {
+Scenario('FeesRegister Verify Pending For Approval And Approve The Fees', async I => {
   I.login('functionaltestapprover@hmcts.net', 'LevelAt12');
   I.wait(CCFRATConstants.fiveSecondWaitTime);
   I.see('Awaiting approval');
--- acceptance-tests/test/end-to-end/tests/CCFeesRegister_EditorScreenValidation_test.js
+++ acceptance-tests/test/end-to-end/tests/CCFeesRegister_EditorScreenValidation_test.js
@@ -5,12 +5,30 @@ const RANDOM_NUMBER = 9999;
 
 Feature('CC FeesRegister Admin Acceptance Tests For Live Fees');
 
-Scenario('FeesRegister Admin Console Editor Header Validation', I => {
+Scenario('FeesRegister Admin Console Editor Header and Tab Validation', I => {
   I.login('functionaltesteditor@hmcts.net', 'LevelAt12');
   I.wait(CCFRATConstants.tenSecondWaitTime);
   I.see("Fees");
+  I.click("Fees");
+  I.waitForText("Live fees","10");
+  I.click("Approved but not live fees");
+  I.waitForText("Approved but not live fees","10");
+  I.see("Code");
+  I.click("Discontinued fees");
+  I.waitForText("Discontinued fees","10");
+  I.see("Code");
   I.see("Your Drafts");
+  I.click("Your Drafts");
+  I.waitForText("Drafts", "10");
+  I.see("Rejected by approver");
+  I.click("Rejected by approver");
+  I.waitForText("Code", "10");
+  I.see("Awaiting approval");
+  I.click("Awaiting approval");
+  I.waitForText("Code", "10");
   I.see("Reference Data");
+  I.click("Reference Data");
+  I.waitForText("Applicants","10");
   I.click('Sign out');
 }).retry(CCFRATConstants.retryScenario);
 
