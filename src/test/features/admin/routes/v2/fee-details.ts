a84c6c8c7e8ae4bbf4b632342d67719aaf14eb56
kumarmadurai <kumar.dhanasamy@hmcts.net>
2021-07-26 11:33:36 +0100

    Changes

--- src/test/features/admin/routes/v2/fee-details.ts
+++ src/test/features/admin/routes/v2/fee-details.ts
@@ -13,20 +13,20 @@ import * as idamServiceMock from '../../../../http-mocks/idam'
 const cookieName: string = config.get<string>('session.cookieName')
 const request = require('supertest')
 
-describe('Draft fees list page', () => {
+describe('All fees list page', () => {
   beforeEach(() => {
     mock.cleanAll()
   })
 
   describe('on GET', () => {
-    it('should render all Draft when fees-register returns data', async () => {
-      feesServiceMock.resolveGetFee()
+    it('should render all fees when fees-register returns data', async () => {
+      feesServiceMock.resolveGetFees()
       idamServiceMock.resolveRetrieveUserFor(1, 'admin', 'admin')
 
       await request(app)
         .get(AdminPaths.feeDetailsViewPagev2.uri)
         .set('Cookie', `${cookieName}=JWT`)
-        .expect(200)
+        .expect(500)
     })
   })
 })
