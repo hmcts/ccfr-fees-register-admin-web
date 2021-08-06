1b459772d5fe264dd080eee3dabff95121ba8566
Pettedson John <pettedson.john@hmcts.net>
2021-08-06 20:01:30 +0100

    Adding the X-browser tests configuration for Fire Fox....

--- acceptance-tests/test/end-to-end/tests/CCFeesRegister_ApproverScreenValidation_test.js
+++ acceptance-tests/test/end-to-end/tests/CCFeesRegister_ApproverScreenValidation_test.js
@@ -2,16 +2,16 @@ const CCFRATConstants = require('./CCFRAcceptanceTestConstants');
 const faker = require('faker');
 const RANDOM_NUMBER = 9999;
 
-/*const approverUserName = process.env.APPROVER_USERNAME;
+const approverUserName = process.env.APPROVER_USERNAME;
 const approverPassword = process.env.APPROVER_PASSWORD;
 
 const editorUserName = process.env.EDITOR_USERNAME;
-const editorPassword = process.env.EDITOR_PASSWORD;*/
+const editorPassword = process.env.EDITOR_PASSWORD;
 
-const approverUserName = 'functionaltestapprover@hmcts.net';
+/*const approverUserName = 'functionaltestapprover@hmcts.net';
 const approverPassword = 'LevelAt12';
 const editorUserName = 'functionaltesteditor@hmcts.net';
-const editorPassword = 'LevelAt12';
+const editorPassword = 'LevelAt12';*/
 
 
 
