3623932e29698b18f28cb6e77b49db7d4fd4fc7a
kumarmadurai <kumar.dhanasamy@hmcts.net>
2021-08-06 09:57:20 +0100

    Changes

--- src/main/features/admin/v2/routes/all-discontinued.ts
+++ src/main/features/admin/v2/routes/all-discontinued.ts
@@ -10,17 +10,22 @@ import { Fee2Dto } from 'fees/v2/model/fees-register-api-contract'
 
 export default express.Router()
   .get(Paths.discontinuedFeesPageV2.uri, (req: express.Request, res: express.Response) => {
-   
+
     FeesClient
-      .searchFees(null, null, null, false, null, false)
-      .then((fees: Array<Fee2Dto>) => {
-        fees.sort((a: Fee2Dto, b: Fee2Dto): number => -a.code.localeCompare(b.code))
-        const feecount  = fees.length;
-        const feesReadable = JSON.stringify(fees)
+    .searchFees(null, null, null, false, null, false)
+    .then((fees: Array<Fee2Dto>) => {
+      fees.sort((a: Fee2Dto, b: Fee2Dto): number => -a.code.localeCompare(b.code))
+
+      FeesClient
+      .searchFees(null, null, null, null, null, false)
+      .then((allfees: Array<Fee2Dto>) => {
+        const feecount  = allfees.length;
+        const feesReadable = JSON.stringify(allfees)
         res.render(Paths.discontinuedFeesPageV2.associatedView, {
           fees: fees,
           feesReadable: feesReadable,
           feecount: feecount
         })
       })
+    })
   })
