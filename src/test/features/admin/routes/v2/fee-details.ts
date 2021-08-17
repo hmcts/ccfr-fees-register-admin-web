45d19157695bbf7f5f8da99e59175cde2ecd90a8
kumarmadurai <kumar.dhanasamy@hmcts.net>
2021-08-17 16:02:36 +0100

    changes

--- src/test/features/admin/routes/v2/fee-details.ts
+++ src/test/features/admin/routes/v2/fee-details.ts
@@ -29,4 +29,29 @@ describe('All fees list page', () => {
         .expect(500)
     })
   })
+  describe('on POST', () => {
+    it('should submit fee version', async () => {
+      feesServiceMock.resolveApprove('FEE002', 1, 'submit')
+      feesServiceMock.resolveGetFees()
+      await request(app)
+        .post(AdminPaths.dashboard.uri)
+        .send({
+          'feeCode': 'FEE002', 'version': 1, 'action': 'submit'
+        })
+        .set('Cookie', `${cookieName}=JWT`)
+        .expect(200)
+    })
+
+    it('should approve fee version', async () => {
+      feesServiceMock.resolveApprove('FEE002', 1, 'approve')
+      feesServiceMock.resolveGetFees()
+      await request(app)
+        .post(AdminPaths.dashboard.uri)
+        .send({
+          'feeCode': 'FEE002', 'version': 1, 'action': 'approve'
+        })
+        .set('Cookie', `${cookieName}=JWT`)
+        .expect(200)
+    })
+  })
 })
