a1c23cc30de994583798d93d00ce12ba3a980ef9
Arbab Ahmed <78796119+arbab-cog@users.noreply.github.com>
2021-08-20 12:38:40 +0100

    Update fees_details.js

--- acceptance-tests/test/end-to-end/pages/fees_details.js
+++ acceptance-tests/test/end-to-end/pages/fees_details.js
@@ -32,6 +32,37 @@ function verifyFeesHeaders(){
   I.waitForText('Memo',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
   I.waitForText('Natural Account Code',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
 }
+function verifyFeesHeadersDiscontinuedFees(){
+  const I = this;
+  I.waitForText('Discontinued fees', CCFRAcceptanceTestConstants.tenSecondWaitTime);
+  I.waitForText('Code',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Description',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Amount',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Statutory Instrument',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('SI Ref ID',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Last Amending SI',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Consolidated/Original Fee Order Name',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Service',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Jurisdiction1',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Jurisdiction2',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Event',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Range from',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Range to',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Unit',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Fee type',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Amount type',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('%',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Channel',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Keyword',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Applicant type',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Version',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Direction',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Valid from',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Valid to',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Status',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Memo',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Natural Account Code',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+}
 function verifyFeeDetails(feeCode, siRefID, consolidatedOriginalFeeOrderName, service, event, description, statutoryInstrument,
 lastAmendingSI, jurisdiction1, jurisdiction2, feeType, amountType, amount, percentage) {
   const I = this;
