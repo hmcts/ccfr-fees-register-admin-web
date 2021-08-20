4aec22f3b94e8ac15ea719fe27afa951f9283d67
chandra-reddy-m <chandra.reddy@hmcts.net>
2021-08-20 10:49:08 +0100

    reverting automating discontinued fee details and bit of refactoring

--- acceptance-tests/test/end-to-end/pages/steps_file.js
+++ acceptance-tests/test/end-to-end/pages/steps_file.js
@@ -4,7 +4,7 @@ const CCPBConstants = require('../tests/CCFRAcceptanceTestConstants');
 // const faker = require('faker');
 const faker = require('faker');
 const RANDOM_NUMBER = 99999;
-const {verifyFeesHeaders, verifyLiveFeeDetails, verifyDiscontinuedFeeDetails, verifyDownloadLink, clickDownloadLink} = require('./fees_details');
+const {verifyFeesHeaders, verifyFeeDetails, verifyDownloadLink, clickDownloadLink} = require('./fees_details');
 const CCDNumber = faker.random.number(RANDOM_NUMBER);
 module.exports = () => actor({
   // done
@@ -168,8 +168,7 @@ module.exports = () => actor({
     },
 
     verifyFeesHeaders,
-    verifyLiveFeeDetails,
-    verifyDiscontinuedFeeDetails,
+    verifyFeeDetails,
     verifyDownloadLink,
     clickDownloadLink
 });
