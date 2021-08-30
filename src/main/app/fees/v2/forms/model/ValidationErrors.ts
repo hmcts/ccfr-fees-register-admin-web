6a04fed5c3733ca53325ec7e6ce37c6c688f4575
kumarmadurai <kumar.dhanasamy@hmcts.net>
2021-08-30 19:46:28 +0100

    alignment issue fix changes

--- src/main/app/fees/v2/forms/model/ValidationErrors.ts
+++ src/main/app/fees/v2/forms/model/ValidationErrors.ts
@@ -39,7 +39,7 @@ export class ValidationErrors {
   static readonly EVENT_REQUIRED: string = 'Select a event'
   static readonly SERVICE_REQUIRED: string = 'Select a service'
   static readonly DIRECTION_REQUIRED: string = 'Select a direction'
-  static readonly REASON_FOR_UPDATE_REQUIRED: string = 'Enter reason for update'
+  static readonly REASON_FOR_UPDATE_REQUIRED: string = 'Enter reason for fee update'
   static readonly APPLICATION_TYPE_REQUIRED: string = 'Select an applicant type'
   static readonly RANGE_UNIT_REQUIRED: string = 'Select a unit for the range'
 }
