548a5f26b5448e4b8e2f3a382589ab9ae547f904
krishnakishorenuthalapati <krishna.nuthalapati@hmcts.net>
2021-10-12 22:52:59 +0100

    Modified functional Tests

--- acceptance-tests/test/end-to-end/tests/CCFeesRegister_EditorScreenValidation_test.js
+++ acceptance-tests/test/end-to-end/tests/CCFeesRegister_EditorScreenValidation_test.js
@@ -52,7 +52,7 @@ Scenario('FeesRegister Admin Console Editor Screen For Live Fees Details', I => 
   I.verifyFeesHeaders();
   //verify any existing fee details under live Tab
   I.verifyFeeDetails('FEE0002','1.2','','divorce','issue','Filing an application for a divorce, nullity or civil partnership dissolution',
-  '2016 No 402','','family','family court','fixed','Flat','550.00','');
+  '2016 No 402','','family','family court','fixed','Flat','593.00','');
   I.click('Sign out');
 }).retry(CCFRATConstants.retryScenario);
 
@@ -79,7 +79,7 @@ Scenario('FeesRegister Admin Console Editor Discontinued Fees Details Check @cro
   I.verifyFeesHeaders();
   //Check one of the existing fee, once full implementation done we can add our own code
   I.verifyFeeDetails('FEE0002','1.2','','divorce','issue','Filing an application for a divorce, nullity or civil partnership dissolution – fees order 1.2.',
-  '2016 No. 402 (L. 5)','','family','family court','fixed','Flat','550.00','');
+  '2016 No. 402 (L. 5)','','family','family court','fixed','Flat','593.00','');
   I.click('Sign out');
 }).retry(CCFRATConstants.retryScenario);
 
