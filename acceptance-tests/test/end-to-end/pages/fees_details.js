cfc0be76c832508f4bbef9cbe5ae2660944f8237
Arbab Ahmed <78796119+arbab-cog@users.noreply.github.com>
2021-10-28 10:13:02 +0100

    functional test

--- acceptance-tests/test/end-to-end/pages/fees_details.js
+++ acceptance-tests/test/end-to-end/pages/fees_details.js
@@ -1,9 +1,39 @@
 'use strict';
 const CCFRAcceptanceTestConstants = require('../tests/CCFRAcceptanceTestConstants');
 
-function verifyFeeDetails(feeTypeFlag, feeCode, siRefID, consolidatedOriginalFeeOrderName, service, event, description, statutoryInstrument,
-                          lastAmendingSI, jurisdiction1, jurisdiction2, feeType, amountType, amount, percentage, validFrom, validTo, version, reasonForFeeUpdate, naturalAccountCode,
-                          memo, direction, applicantType, keyword, channel, status, editor, approver) {
+function verifyFeesHeaders(){
+  const I = this;
+  I.waitForText('Live fees', CCFRAcceptanceTestConstants.tenSecondWaitTime);
+  I.waitForText('Code',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Description',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Amount',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Statutory Instrument',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('SI Ref ID',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Last Amending SI',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Consolidated/Original Fee Order Name',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Service',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Jurisdiction1',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Jurisdiction2',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Event',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Range from',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Range to',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Unit',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Fee type',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Amount type',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('%',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Channel',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Keyword',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Applicant type',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Version',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Direction',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Valid from',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Valid to',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Status',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Memo',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+  I.waitForText('Natural Account Code',CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+}
+function verifyFeeDetails(feeCode, siRefID, consolidatedOriginalFeeOrderName, service, event, description, statutoryInstrument,
+lastAmendingSI, jurisdiction1, jurisdiction2, feeType, amountType, amount, percentage) {
   const I = this;
   I.click(feeCode);
   I.waitForText('Fee details', CCFRAcceptanceTestConstants.tenSecondWaitTime);
@@ -34,38 +64,32 @@ function verifyFeeDetails(feeTypeFlag, feeCode, siRefID, consolidatedOriginalFee
   I.see('Amount');
   I.see('£'+amount);
   I.see('Percentage');
-  I.see(percentage);
   I.see('Valid from');
-  I.see(validFrom);
-
-  if(feeTypeFlag === 'discontinued') {
-    I.see('Valid to');
-    I.see(validTo);
-  }
   I.see('Version');
-  I.see(version);
   I.see('Reason for fee update');
-  I.see(reasonForFeeUpdate);
   I.see('Natural account code');
-  I.see(naturalAccountCode);
   I.see('Memo');
-  I.see(memo);
   I.see('Direction');
-  I.see(direction);
   I.see('Applicant type');
-  I.see(applicantType);
   I.see('Keyword');
-  I.see(keyword);
   I.see('Channel');
-  I.see(channel);
   I.see('Status');
-  I.see(status);
   I.see('Editor');
-  I.see(editor);
   I.see('Approver');
-  I.see(approver);
+  I.see('test editor');
+}
+
+function verifyDownloadLink(){
+  const I = this;
+  I.waitForValue('//input','Download all fees', CCFRAcceptanceTestConstants.fiveSecondWaitTime);
+}
+
+function clickDownloadLink(){
+  const I = this;
+  I.click('Download all fees');
+  I.wait(CCFRAcceptanceTestConstants.fiveSecondWaitTime);
 }
 
 module.exports = {
-  verifyFeeDetails
+  verifyFeesHeaders , verifyFeeDetails, verifyDownloadLink, clickDownloadLink
 };
