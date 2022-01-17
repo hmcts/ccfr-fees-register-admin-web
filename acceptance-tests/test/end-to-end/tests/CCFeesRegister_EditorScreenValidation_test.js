4c84a838bd44d306a0d1832dcf770732033436ab
krishnakishorenuthalapati <krishna.nuthalapati@hmcts.net>
2022-01-17 17:02:58 +0000

    Modified direction data in FT

--- acceptance-tests/test/end-to-end/tests/CCFeesRegister_EditorScreenValidation_test.js
+++ acceptance-tests/test/end-to-end/tests/CCFeesRegister_EditorScreenValidation_test.js
@@ -54,7 +54,7 @@ Scenario('FeesRegister Admin Console Editor Screen For Live Fees Details', I => 
   //verify any existing fee details under live Tab
   I.verifyFeeDetails('live', 'FEE0002','1.2','Family Proceedings Fees Order 2008','divorce','issue','Filing an application for a divorce, nullity or civil partnership dissolution',
     '2021 No 985','The Court Fees (Miscellaneous Amendments) Order 2021','family','family court','fixed','Flat','593.00','',
-    '30 September 2021', '', '6', 'Inflationary Increase', '4481102159', 'RECEIPT OF FEES - Family issue divorce', 'enhanced', 'all', 'DivorceCivPart', 'default', 'approved', 'cef56daa-572c-464b-bd32-4a487c771d47', '39907');
+    '30 September 2021', '', '6', 'Inflationary Increase', '4481102159', 'RECEIPT OF FEES - Family issue divorce', 'inflationary', 'all', 'DivorceCivPart', 'default', 'approved', 'cef56daa-572c-464b-bd32-4a487c771d47', '39907');
   I.click('Sign out');
 }).retry(CCFRATConstants.retryScenario);
 
