b1cd0e536ef9cec9407146516f73bc6f03d4f780
“PragneshPatel2” <pragnesh.patel2@hmcts.net>
2022-02-08 09:11:02 +0000

    PAY-4720 Cookie Banner- Dynatrace

--- gulpfile.js
+++ gulpfile.js
@@ -104,3 +104,25 @@ gulp.task('default', gulp.series('sass',
   'develop',
   'watch'
 ));
+
+function copyCookieBanner() {
+  gulp.src([
+    `./node_modules/cmc-cookies-manager/shared-component/components/cookie-manager/**/*.js`
+  ])
+    .pipe(gulp.dest(`${assetsDirectory}/js/`))
+
+  gulp.src([
+    `./node_modules/cmc-cookies-manager/shared-component/components/cookie-manager/**/*.njk`
+  ])
+    .pipe(gulp.dest(`${appDirectory}/cookie-manager/`))
+
+  gulp.src([
+    `./node_modules/cmc-cookies-manager/shared-component/components/button/**/*.*`
+  ])
+    .pipe(gulp.dest(`${appDirectory}/button/`))
+
+  gulp.src([
+    `./node_modules/cmc-cookies-manager/shared-component/components/cookie-banner/**/*.*`
+  ])
+    .pipe(gulp.dest(`${appDirectory}/cookie-banner/`))
+}
