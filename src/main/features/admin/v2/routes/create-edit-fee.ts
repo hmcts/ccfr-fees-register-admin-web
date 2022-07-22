680ab0bd89a3fa9d52de16ab24290d7ae7ce90af
“PragneshPatel2” <pragnesh.patel2@hmcts.net>
2022-07-22 11:28:51 +0100

    Pay 5683 chart version upgrade

--- src/main/features/admin/v2/routes/create-edit-fee.ts
+++ src/main/features/admin/v2/routes/create-edit-fee.ts
@@ -28,7 +28,7 @@ class Renderer {
 export default express.Router()
   .get(Paths.feeCreatePageV2.uri, (req: express.Request, res: express.Response) => {
     if (req.query.action === 'edit' && req.query.feeCode) {
-      FeesClient.getFee(res.locals.user, req.query.feeCode)
+      FeesClient.getFee(res.locals.user as any, req.query.feeCode as any)
         .then(fee => {
           let reasonForReject = fee.fee_versions[0].reason_for_reject
           let approvedBy = fee.fee_versions[0].approvedBy
