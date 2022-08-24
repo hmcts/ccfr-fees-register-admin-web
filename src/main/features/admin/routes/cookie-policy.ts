e45eeb8176eb2d9fcbd378bc215beb5d78c40953
“akhil-nuthakki” <akhil.nuthakki@HMCTS.net>
2022-08-24 09:58:24 +0100

    Cookie-Banner

---
+++ src/main/features/admin/routes/cookie-policy.ts
@@ -0,0 +1,17 @@
+import * as express from 'express'
+
+import { Paths } from 'admin/paths'
+
+import { CookiePolicy } from 'app/cookie-banner/cookiePolicy'
+import { Category } from 'app/cookie-banner/category'
+
+export default express.Router()
+.get(Paths.cookiePolicyPage.uri, (req: express.Request, res: express.Response) => {
+        
+        res.render(Paths.cookiePolicyPage.associatedView, { 
+            countCookiesByCatGoogle: CookiePolicy.countCookies(Category.GOOGLE),
+            countCookiesByCatDynatrace: CookiePolicy.countCookies(Category.DYNATRACE),
+            cookiesByCatGoogle: CookiePolicy.cookiesByCat(Category.GOOGLE),
+            cookiesByCatDynatrace: CookiePolicy.cookiesByCat(Category.DYNATRACE) 
+        })
+    })
