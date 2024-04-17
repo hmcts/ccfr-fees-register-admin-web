facec993af6242e8ed3166ea009cf05bf2f2c513
StoeBenHMCTS <Ben.Stone@HMCTS.NET>
2024-04-17 17:04:53 +0100

    changes the local port

--- src/main/server.ts
+++ src/main/server.ts
@@ -12,7 +12,7 @@ import { AppInsights } from './app-insights/app-insights'
 // App Insights needs to be enabled as early as possible as it monitors other libraries as well
 AppInsights.enable()
 
-const port: string = process.env.PORT || '3000'
+const port: string = process.env.PORT || '3001'
 
 if (app.locals.ENV === 'development') {
   const sslDirectory = path.join(__dirname, 'resources', 'localhost-ssl')
