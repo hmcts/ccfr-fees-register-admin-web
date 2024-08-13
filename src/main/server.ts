8d0cf7ff4deb2a2c9268138503844dd7474dbc7b
Ollie-Smith <oliver.smith@hmcts.net>
2024-08-13 08:33:50 +0100

    PAY-7120 addressed VUL-6113

--- src/main/server.ts
+++ src/main/server.ts
@@ -4,16 +4,28 @@ import * as config from 'config'
 import * as secrets from 'modules/secrets'
 secrets.setup(config)
 import { app } from './app'
+import * as fs from 'fs'
+import * as path from 'path'
+import * as https from 'https'
 import { AppInsights } from './app-insights/app-insights'
 
 // App Insights needs to be enabled as early as possible as it monitors other libraries as well
 AppInsights.enable()
 
-let port: string = process.env.PORT || '3000'
+const port: string = process.env.PORT || '3001'
 
-if (process.env.NODE_ENV === 'development') {
-  port = '3001'
+if (app.locals.ENV === 'development') {
+  const sslDirectory = path.join(__dirname, 'resources', 'localhost-ssl')
+  const sslOptions = {
+    key: fs.readFileSync(path.join(sslDirectory, 'localhost.key')),
+    cert: fs.readFileSync(path.join(sslDirectory, 'localhost.crt'))
+  }
+  const server = https.createServer(sslOptions, app)
+  server.listen(port, () => {
+    console.log(`Application started: https://localhost:${port}`)
+  })
+} else {
+  app.listen(port, () => {
+    console.log(`Application started: http://localhost:${port}`)
+  })
 }
-app.listen(port, () => {
-  console.log(`Application started: http://localhost:${port}`)
-})
