06b04db309a11190c5a5e0c5c80cfc996ca8f932
Chris Worthington <Christopher.Worthington@HMCTS.NET>
2025-03-12 14:01:19 +0000

    PAY-6695 add active link styling support to tabgroup on draft pages

--- src/main/public/js/styles.js
+++ src/main/public/js/styles.js
@@ -1,10 +1,11 @@
 function styleActiveLink() {
   const navLinks = document.querySelectorAll('div.header-proposition ul.custom-header-ul a');
   navLinks.forEach((anchor) => {
-    console.log(anchor);
-    if (anchor.href === window.location.href) {
+    if (anchor.href === window.location.href || anchor.href.includes('draft') && window.location.href.includes('draft')) {
       anchor.style.color = '#1d8feb';
-    } else {
+    }
+    else
+    {
       anchor.style.color = 'white';
     }
   });
