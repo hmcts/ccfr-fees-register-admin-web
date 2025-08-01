5014dae08742b6362b5f6063875b6b65086a7f78
Dave Jones <dave.jones@hmcts.net>
2025-08-01 22:01:47 +0100

    PAY-7543: Refactor code to use gettext-converter instead of i18next-conv

--- src/main/modules/i18n/backend.js
+++ src/main/modules/i18n/backend.js
@@ -1,67 +1,48 @@
-"use strict";
+'use strict';
+const { readFile } = require('fs');
+const { po2i18next } = require('gettext-converter'); // replace i18next-conv
 
-const {readFile} = require('fs');
-
-const converter = require('i18next-conv');
-
-/**
- A gettext backend for i18next framework
- */
 class Backend {
+  static type = 'backend';
 
   static defaultsOptions() {
-    return {
-      loadPath: '/locales/{{lng}}/{{ns}}.po'
-    }
+    return { loadPath: '/locales/{{lng}}/{{ns}}.po' };
   }
 
   constructor(services, options = {}) {
-    this.init(services, options)
+    this.init(services, options);
   }
 
-  /**
-   * Initializes backend (required by framework)
-   * @param services - available services
-   * @param options - backend options
-   * @param coreOptions - i18next options
-   */
   init(services, options = {}, coreOptions = {}) {
-    this.services = services
-    this.options = Object.assign({}, Backend.defaultsOptions(), options)
-    this.coreOptions = coreOptions
+    this.services = services;
+    this.options = Object.assign({}, Backend.defaultsOptions(), options);
+    this.coreOptions = coreOptions;
   }
 
   /**
-   * Reads translations from PO file (required by framework)
-   * @param language - language to be loaded
-   * @param namespace - namespace to be used
-   * @param callback - standard Node.js callback function (err, data)
+   * Reads translations from a PO file and converts them to i18next format.
+   * @param {string} language - The language code (e.g., 'en', 'cy').
+   * @param {string} namespace - The namespace for the translations.
+   * @param {function} callback - Callback function to handle the result.
    */
   read(language, namespace, callback) {
-
     const translationFile = this.options.loadPath.replace(/{{lng}}/, language).replace(/{{ns}}/, namespace);
 
-    readFile(translationFile, function (err, data) {
-
+    readFile(translationFile, 'utf8', function (err, data) {
       if (err) {
         return callback(err, null);
       }
 
-      converter
-        .gettextToI18next(language, data)
-        .then(
-          function (translation) {
-            callback(null, JSON.parse(translation));
-          }
-        ).catch(
-        function (err) {
-          callback(err, null);
-        }
-      );
-    })
-  }
-}
+      try {
+        const result = po2i18next(data, { compatibilityJSON: 'v4' });
+        callback(null, result); // returns { translation: { ... } }
+      } catch (parseError) {
+        callback(parseError, null);
+      }
+    });
 
-Backend.type = "backend";
+  }
+
+}
 
 module.exports = Backend;
