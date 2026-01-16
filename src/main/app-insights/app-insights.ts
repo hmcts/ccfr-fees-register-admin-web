1b24f76307844b32a984ae0e73d1cabcfbffe27f
Sammi Chong <133241942+SammiChong@users.noreply.github.com>
2026-01-16 16:54:42 +0000

    Remove /health exclusion

--- src/main/app-insights/app-insights.ts
+++ src/main/app-insights/app-insights.ts
@@ -9,10 +9,6 @@ export class AppInsights {
     appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = config.get<string>('appInsights.roleName')
     appInsights.defaultClient.config.samplingPercentage = 1
 
-    appInsights.defaultClient.addTelemetryProcessor((envelope) =>
-      !envelope.data?.baseData?.name?.includes('GET /health')
-    )
-
     appInsights.start()
   }
 }
