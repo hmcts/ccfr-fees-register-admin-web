ceb1a18802195a190d42d75521cd15bbd0def12e
Chris Worthington <Christopher.Worthington@HMCTS.NET>
2026-01-19 16:06:12 +0000

    CME-847 only sample /health

--- src/main/app-insights/app-insights.ts
+++ src/main/app-insights/app-insights.ts
@@ -1,13 +1,24 @@
 import * as config from 'config'
 import * as appInsights from 'applicationinsights'
 
+function fineGrainedSampling (envelope: appInsights.Contracts.EnvelopeTelemetry): boolean {
+  if (
+    ['RequestData', 'RemoteDependencyData'].includes(envelope.data.baseType) &&
+    envelope.data.baseData.name.includes('/health')
+  ) {
+    envelope.sampleRate = 1
+  }
+
+  return true
+}
+
 export class AppInsights {
   static enable () {
     appInsights.setup(config.get<string>('appInsights.instrumentationKey'))
       .setAutoDependencyCorrelation(true)
       .setAutoCollectConsole(true, true)
     appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = config.get<string>('appInsights.roleName')
-    appInsights.defaultClient.config.samplingPercentage = 1
+    appInsights.defaultClient.addTelemetryProcessor(fineGrainedSampling)
 
     appInsights.start()
   }
