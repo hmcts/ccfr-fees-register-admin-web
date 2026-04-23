7de8b2aac3cde7243ce4b0e8c7f872b668ea9dcb
GraemeB <graeme.bullimore@hmcts.net>
2026-04-23 12:07:10 +0100

    PAY-8683: fix mocharc config

--- .mocharc.js
+++ .mocharc.js
@@ -5,10 +5,12 @@ module.exports = {
     timeout: 10000,
     recursive: true,
     extension: ['ts', 'js'],
-    loader: 'ts-node/esm',
+    'node-option': [
+      'loader=./node_modules/ts-node/esm.mjs'
+    ],
     require: [
       'ts-node/register',
-      'tsconfig-paths/register'
+      'tsconfig-paths/register.js'
     ],
     reporter: 'mochawesome'
 }
